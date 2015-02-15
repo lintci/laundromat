42816f7cc6b81120b35d2c129d097f51111f4e30
Allen Madsen <blatyo@gmail.com>
2015-02-15 18:03:27 -0500

    Remove urls from payload to decrease the size of log messages

--- lib/worker_logger.rb
+++ lib/worker_logger.rb
@@ -6,13 +6,34 @@ class WorkerLogger
       finished: finished,
       duration: finished - started,
       notification: unique_id,
-      payload: payload
+      payload: strip_urls(payload)
     }
 
     if Sidekiq.server?
       Sidekiq.logger.info(data)
     else
       Rails.logger.info(data)
+    end
+  end
+
+private
+
+  def strip_urls(payload)
+    payload.reduce({}) do |new_payload, (key, value)|
+      case value
+      when String
+        new_payload[key] = value unless key =~ /_url$/
+      when Hash
+        new_payload[key] = strip_urls(payload)
+      when Array
+        new_payload[key] = if value.first.is_a?(Hash)
+          value.map{|v| strip_urls(v)}
+        else
+          value
+        end
+      else
+        new_payload[key] = value
+      end
     end
   end
 end
