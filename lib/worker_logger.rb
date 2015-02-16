481883e8c76b19d926ee60cf7c4da5f1d5b2eec2
Allen Madsen <blatyo@gmail.com>
2015-02-15 21:18:47 -0500

    Fix logger

--- lib/worker_logger.rb
+++ lib/worker_logger.rb
@@ -9,6 +9,8 @@ class WorkerLogger
       payload: strip_urls(payload)
     }
 
+    puts data[:payload]
+
     if Sidekiq.server?
       Sidekiq.logger.info(data)
     else
@@ -19,20 +21,13 @@ class WorkerLogger
 private
 
   def strip_urls(payload)
-    payload.reduce({}) do |new_payload, (key, value)|
-      case value
-      when String
-        new_payload[key] = value unless key =~ /_url$/
-      when Hash
-        new_payload[key] = strip_urls(payload)
-      when Array
-        new_payload[key] = if value.first.is_a?(Hash)
-          value.map{|v| strip_urls(v)}
-        else
-          value
-        end
+    payload.each_pair.with_object({}) do |(key, value), new_payload|
+      next if key =~ /_url$/
+
+      new_payload[key] = if value.is_a?(Hash)
+        strip_urls(value)
       else
-        new_payload[key] = value
+        value
       end
     end
   end
