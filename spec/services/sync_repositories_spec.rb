58c729c5c9604777d412fdd1abeee7c8252dc55b
Allen Madsen <blatyo@gmail.com>
2015-07-05 09:36:32 -0400

    Optionally set environment variables used for initial requests done with VCR.

--- spec/services/sync_repositories_spec.rb
+++ spec/services/sync_repositories_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe SyncRepositories do
-  let(:access_token){create(:access_token, provider_token: ENV.fetch('GITHUB_USER_ACCESS_TOKEN'))}
+  let(:access_token){create(:access_token, provider_token: ENV['GITHUB_USER_ACCESS_TOKEN'])}
   let(:user){create(:user, access_tokens: [access_token])}
   let(:provider){Provider[:github]}
   subject(:service){described_class}
--- spec/support/env.rb
+++ spec/support/env.rb
@@ -1,1 +1,3 @@
-ENV['SSH_PASSPHRASE'] = 'TurtleSocks'
+ENV['SSH_PASSPHRASE'] ||= 'TurtleSocks'
+ENV['GITHUB_CLIENT_ID'] ||= 'MonkeySocks'
+ENV['GITHUB_CLIENT_SECRET'] ||= 'DoveSocks'
