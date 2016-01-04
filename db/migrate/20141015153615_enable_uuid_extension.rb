8286eb17ee3cffe7942cbe9bc40beec5cce8295e
Allen Madsen <blatyo@gmail.com>
2016-01-03 22:17:36 -0500

    Only setup extension for dev. Production requires super user.

--- db/migrate/20141015153615_enable_uuid_extension.rb
+++ db/migrate/20141015153615_enable_uuid_extension.rb
@@ -1,5 +1,5 @@
 class EnableUuidExtension < ActiveRecord::Migration
   def change
-    enable_extension 'uuid-ossp'
+    enable_extension 'uuid-ossp' if Rails.env.test? || Rails.env.development?
   end
 end
